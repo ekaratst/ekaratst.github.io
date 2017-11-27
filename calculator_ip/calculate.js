91b80590399c0031492864ef46f32d865838c3f8
ekarat.st <ekarat.s@ku.th>
2017-11-27 17:02:11 +0700

    alert

--- calculator_ip/calculate.js
+++ calculator_ip/calculate.js
@@ -17,7 +17,31 @@ function convertSubnetCIDR(subnetCIDR){
     }
     return result.join('.');
 }
+
+function ValidateIPaddress(ipaddress)   
+{  
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress))  
+    {  
+        return true;
+    }  
+    return false;
+}  
+
 for(var i =1; i<=32; i++)
 {
     $("select.subnet").append("<option value=\""+ i +"\" >"+ convertSubnetCIDR(i) + '/' + i +"</option>")
 }
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    var rawdata = $(this).serializeArray();
+    var data = {}; 
+    for(var i = 0; i<rawdata.length; i++){
+        data[rawdata[i].name] = rawdata[i].value;
+    }
+    console.log(data);
+    if(!ValidateIPaddress(data.ip)){
+        alert("Please provide a valid IP address.");
+        return;
+    }
+});
