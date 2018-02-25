dabb2e480342882e0f442f2102ae4d405c50d536
Suttipat Sukwattanakorn <suttipat.new@gmail.com>
2018-02-25 21:14:29 +0700

    connect with function.js

--- substitution-cipher/assets/js/function.js
+++ substitution-cipher/assets/js/function.js
@@ -5,41 +5,19 @@ function caesarShift(input, amount) {
 		if (str.match(/[a-z]/i)) {
 			var code = input.charCodeAt(i);
 			if ((code >= 65) && (code <= 90))
-				str = input.fromCharCode(((code - 65 + amount) % 26) + 65);
+				str = String.fromCharCode(((code - 65 + amount) % 26) + 65);
 			else if ((code >= 97) && (code <= 122))
-				str = input.fromCharCode(((code - 97 + amount) % 26) + 97);
+				str = String.fromCharCode(((code - 97 + amount) % 26) + 97);
 		}
 		output += str;
 	}
 	return output;
 }
 
-function substitution(input, ) {
-    var map = {
-        a: 'q', b: 'w', c: 'e',
-        d: 'r', e: 't', f: 'y',
-        g: 'u', h: 'i', i: 'o',
-        j: 'p', k: 'a', l: 's',
-        m: 'd', n: 'f', o: 'g',
-        p: 'h', q: 'j', r: 'k',
-        s: 'l', t: 'z', u: 'x',
-        v: 'c', w: 'v', x: 'b',
-        y: 'n', z: 'm'
-    };
-    if(decode) {
-        map = (function() {
-            var tmp = {};
-            var k;
-            for(k in map) {
-                if(!map.hasOwnProperty(k)) continue;
-                tmp[map[k]] = k;
-            }
-            return tmp;
-        })();
+function substitution(input, map) {
+    var output = "";
+    for (var i in input) {
+        output += map[input[i]];
     }
-    return input.split('').filter(function(v) {
-        return map.hasOwnProperty(v.toLowerCase());
-    }).map(function(v) {
-        return map[v.toLowerCase()].toUpperCase();
-    }).join('');
+    return output;
 };
--- substitution-cipher/assets/js/initial.js
+++ substitution-cipher/assets/js/initial.js
@@ -1,6 +1,6 @@
 function initJulius(elementId) {
     for (var i = 1; i <= 25; i++) {
-        $('ul#' + elementId).append('<li role="presentation" value="' + i + '"><a href="#">' + i + '</a></li>');
+        $('select#' + elementId).append('<option value="' + i + '"><a href="#">' + i + '</a></li>');
     }
 }
 
@@ -8,14 +8,14 @@ function initSimpleSubCipher(elementId) {
     var A_ascii = 'A'.charCodeAt(0);
     var $alphabetsOption = $('<select class="form-control alphabet-select" style="width: 60px; display: inline-block;"></select>');
     for (var i = 0; i < 26; i++) {
-        $alphabetsOption.append('<option>' + String.fromCharCode(A_ascii + i) + '</option>');
+        $alphabetsOption.append('<option value="' + String.fromCharCode(A_ascii + i) + '">' + String.fromCharCode(A_ascii + i) + '</option>');
     }
     var $row = $('<div class="row"></div>');
     for (var i = 0; i < 26; i++) {
         var $div = $('<div class="col-xs-3" ></div>');
         $div.append('<div style="width: 40px; display: inline-block;">' + String.fromCharCode(A_ascii + i) + ' =&gt; </div>');
         $div.append($alphabetsOption.clone());
-        $div.find('select').attr('name', 'alphabet' + String.fromCharCode(A_ascii + i))
+        $div.find('select').attr('id', 'alphabet' + String.fromCharCode(A_ascii + i))
         $row.append($div);
         if ((i+1) % 4 == 0 && i !== 0) {
             $('#' + elementId).append($row);
@@ -28,4 +28,19 @@ function initSimpleSubCipher(elementId) {
 $(document).ready(function() {
     initJulius('julius-caesar-select');
     initSimpleSubCipher("simple-substitution");
+    $('#encode').click(function() {
+        var inputText = $('.input_source').val();
+        if ($('input#julius-checkbox').prop('checked')) {
+            var shiftAmout = Number($('#julius-caesar-select').val());
+            var result = caesarShift(inputText, shiftAmout);
+        } else {
+            var A_ascii = 'A'.charCodeAt(0);
+            var map = {};
+            for (var i = 0; i < 26; i++) {
+                map[String.fromCharCode(A_ascii + i)] = $('#alphabet'+String.fromCharCode(A_ascii + i)).val();
+            }
+            var result = substitution(inputText, map);
+        }
+        $('.output_result').val(result);
+    });
 });
